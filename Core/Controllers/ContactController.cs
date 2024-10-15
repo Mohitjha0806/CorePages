a43361dd8dbb72c07d77e80e081c470dc4ec8526
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-10-15 17:31:49 +0530

    Add project files.

---
+++ Core/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+    }
+}
